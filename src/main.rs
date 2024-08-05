fa5797fecefb3149131dcb3111a9d5d4d0b8ef56
Sese Mueller <sese4dasbinichagmail.com>
2024-08-05 15:18:06 +0200

    Corrected Ping Response and added it as a trace at the start of the program.

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,8 @@ async fn main() -> std::io::Result<()> {
     let _ = chatbot::prompting::STARTING_PROMPT_JSON.clone();
     trace!("Starting messages JSON: {:?}", chatbot::prompting::STARTING_PROMPT_JSON);
 
+    trace!("Ping Response: {:?}", static_serve::RESPONSE);
+
     // We'll also initialize the authentication here so it's available for the entire server, from the very start.
     let auth_string = match std::env::var("AUTH_KEY"){
         Ok(auth_string) => auth_string,
--- src/static_serve.rs
+++ src/static_serve.rs
@@ -2,24 +2,27 @@
 
 use actix_web::{HttpResponse, Responder};
 use const_format::concatcp;
+use tracing::{debug, trace};
 
 // The Ping reponse contains a short description of the server's capabilities.
+
+
+const VERSION: &str = concat!("Version: ", env!("CARGO_PKG_VERSION"), "\n");
+const STREAMVARIANTS: &str = "Streamvariants=User,Assistant,Code,CodeOutput,Image,ServerError,OpenAIError,CodeError,StreamEnd,ServerHint\n"; //TODO: un-hardcode this, instead read the variants from the LLM module
+const PING_SPEC:&str ="ping:get,,String\n"; // ping has the get method, no input and outputs String.
+const GETTHREAD_SPEC:&str="getthread:get,thread_id=String&auth_key=String,Json{List{Variant:Streamvariant=String,Content:String}}\n"; // getthread has the get method, takes a threadid and returns a list of Streamvariants and their content.
+const STREAMRESPONSE_SPEC:&str="streamresponse:get,thread_id=Optional{String}&input=String&auth_key=String,Stream{Json{Variant:Streamvariant=String,Content:String}}\n"; // streamresponse has the get method, takes an optional threadid and returns a stream of Streamvariants and their content.
+const STOP_SPEC:&str = "stop:post+get,thread_id=String&auth_key=String,\n"; // stop can be called with post or get, takes a threadid and returns nothing.
+const ALL_SPECS :&str = concatcp!(PING_SPEC,GETTHREAD_SPEC,STREAMRESPONSE_SPEC,STOP_SPEC);
+pub const RESPONSE: &str = concatcp!(VERSION, STREAMVARIANTS, ALL_SPECS);
+
 pub async fn ping() -> impl Responder {
-
-
-    const VERSION: &str = concat!("Version: ", env!("CARGO_PKG_VERSION"), "\n");
-    const STREAMVARIANTS: &str = "Streamvariants=User,Assistant,Code,CodeOutput,Image,ServerError,OpenAIError,CodeError,StreamEnd,ServerHint\n"; //TODO: un-hardcode this, instead read the variants from the LLM module
-    const PING_SPEC:&str ="ping:get,,String\n"; // ping has the get method, no input and outputs String.
-    const GETTHREAD_SPEC:&str="getthread:get,thread_id=String,Json{List{Variant:Streamvariant=String,Content:String}}\n"; // getthread has the get method, takes a threadid and returns a list of Streamvariants and their content.
-    const STREAMRESPONSE_SPEC:&str="streamresponse:get,thread_id=Optional{String},Stream{Json{Variant:Streamvariant=String,Content:String}}\n"; // streamresponse has the get method, takes an optional threadid and returns a stream of Streamvariants and their content.
-    const STOP_SPEC:&str = "stop:post+get,thread_id=String,\n"; // stop can be called with post or get, takes a threadid and returns nothing.
-    const ALL_SPECS :&str = concatcp!(PING_SPEC,GETTHREAD_SPEC,STREAMRESPONSE_SPEC,STOP_SPEC);
-    const RESPONSE: &str = concatcp!(VERSION, STREAMVARIANTS, ALL_SPECS);
-
+    trace!("Ping request received.");
     HttpResponse::Ok().body(RESPONSE)
 }
 
 // not_found returns a 404 response
 pub async fn not_found() -> impl Responder {
+    debug!("404 Method Not Found, try /help");
     HttpResponse::NotFound().body("404 Method Not Found, try /help")
 }
