d115fd258f07e694891ee84b15631b96992803ec
Miklip  Evaluationsystem <b380001@levante3.atos.local>
2024-10-30 17:50:23 +0100

    remove old deployment scripts, as they have become deprecated

--- podman-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-podman build --cgroup-manager=cgroupfs -t freva-gpt2-backend -f dockerfiles/chatbot-backend/Dockerfile .
--- start_ollama.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-podman run -d --device nvidia.com/gpu=all -v ollama:/home/b/b380001/ollama-podman/.ollama --network freva-gpt --name ollama ollama/ollama
